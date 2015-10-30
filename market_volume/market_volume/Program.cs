d2b75e28b1a5c985398e83f79e6097ebae66742b
n0maddd <zolvanoff@mail.ru>
2015-10-30 17:58:51 +0300

    Add input of data for 2 years

--- market_volume/market_volume/Program.cs
+++ market_volume/market_volume/Program.cs
@@ -13,6 +13,24 @@ namespace Statistic
             Console.WriteLine("\n\t//////////////////");
             Console.WriteLine("\t/// STATISTICS ///");
             Console.WriteLine("\t//////////////////\n");
+
+            // Zero period
+            int Year0 = 2013;
+
+            Console.WriteLine(String.Format("Enter price in {0} year:", Year0));
+            decimal Price0 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(String.Format("Number of items sold in {0} year:", Year0));
+            decimal NumberOfItems0 = Convert.ToDecimal(Console.ReadLine());
+
+            // Next period
+            int Year1 = 2014;
+
+            Console.WriteLine(String.Format("Enter price in {0} year:", Year1));
+            decimal Price1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(String.Format("Number of items sold in {0} year:", Year1));
+            decimal NumberOfItems1 = Convert.ToDecimal(Console.ReadLine());
         }
     }
 }
