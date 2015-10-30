0fe84304b3e25b928c4b04297592e2844ab25327
n0maddd <zolvanoff@mail.ru>
2015-10-30 18:17:20 +0300

    Add result output

--- market_volume/market_volume/Program.cs
+++ market_volume/market_volume/Program.cs
@@ -32,6 +32,10 @@ namespace Statistic
             Console.WriteLine(String.Format("Number of items sold in {0} year:", Year1));
             decimal NumberOfItems1 = Convert.ToDecimal(Console.ReadLine());
 
+            // Status output
+            Console.WriteLine("\nThank you! Data is entered.");
+            Console.WriteLine("Calculation is performed...\n");
+
             // Calculate volumes
             decimal MarketVolume0 = Price0 * NumberOfItems0;
             decimal MarketVolume1 = Price1 * NumberOfItems1;
@@ -42,6 +46,37 @@ namespace Statistic
 
             // Calculate volume delta
             decimal DeltaMarketVolume = (MarketVolume1 - MarketVolume0) * 100 / MarketVolume0;
+
+            // Output table
+            //  Header
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine(" YEAR | C\tC %\tM\tM %\tE\t\tE %");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            //  Data
+
+            Console.WriteLine(String.Format(" {0} | {1}\t\t{2}\t\t{3}\t\t",
+                Year0,
+                String.Format("{0:0.##}", Price0),
+                String.Format("{0:0.##}", NumberOfItems0),
+                String.Format("{0:0.##}", MarketVolume0)));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            Console.WriteLine(String.Format(" {0} | {1}\t{2}\t{3}\t{4}\t{5}\t\t{6}",
+                Year1,
+                String.Format("{0:0.##}", Price1),
+                String.Format("{0:0.##}", DeltaPrice),
+                String.Format("{0:0.##}", NumberOfItems1),
+                String.Format("{0:0.##}", DeltaNumberOfItems),
+                String.Format("{0:0.##}", MarketVolume1),
+                String.Format("{0:0.##}", DeltaMarketVolume)));
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            Console.WriteLine("\n\t//////////////");
+            Console.WriteLine("\t/// FINISH ///");
+            Console.WriteLine("\t//////////////\n");
+
+            Console.Read();
         }
     }
 }
